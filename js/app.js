6feb3c701cbd8bdf6421c4519e3f2d425f1ad5dd
JudyVue <judyvue87@gmail.com>
2016-07-25 23:10:43 -0700

    Day 6 to turn in

--- js/app.js
+++ js/app.js
@@ -26,27 +26,35 @@ var firstPike = {
       }
     },
     render: function() {
+      //naming my list
       var List = document.createElement('ul');
-
+      List.textContent = 'First and Pike';
+      //first for loop iterates through the hours
       for (var i = 0; i < hours.length; i++){
         var hoursListElement = document.createElement('li');
-        //hoursListElement.textContent = hours[i];
-        //List.appendChild(hoursListElement)
-
+        hoursListElement.textContent = hours[i];
+        //second for loop iterates through the cookes per hours
         for (var j = 0; j < firstPike.cookiesEachHourArray.length; j++) {
           var cookiesListElement = document.createElement('li');
           cookiesListElement.textContent = hours[i] + ': ' + firstPike.cookiesEachHourArray[i] + ' cookies';
           List.appendChild(cookiesListElement);
+          break; //this makes it so that hour:cookies doesn't repeat 14 times each
         }
-
-  }
-  document.body.appendChild(List);
-}
-
+      }
+      //appends the total# of cookies sold per day to end of list
+      var totalDailyCookieSales = this.totalDailyCookieSales;
+      var totalSalesElement = document.createElement('li');
+      totalSalesElement.textContent = 'Total: ' + totalDailyCookieSales + ' cookies';
+      var position = document.getElementsByTagName('ul')[15];
+      List.appendChild(totalSalesElement);
+      document.body.appendChild(List);
+  },
 };
 
 firstPike.salesPerHour();
 firstPike.render();
+
+
 //Second store: SeaTac Airport, min cust = 3, max cust = 24, avg = 1.2
 var seaTacAirport = {
     minCust: 3,
@@ -62,11 +70,36 @@ var seaTacAirport = {
         this.cookiesEachHourArray.push(cookiesPerHour);
         this.custEachHourArray.push(custPerHour);
         this.totalDailyCookieSales += cookiesPerHour;
-
       }
-    }
+    },
+    render: function() {
+      //naming my list
+      var List = document.createElement('ul');
+      List.textContent = 'SeaTac Airport';
+      //first for loop iterates through the hours
+      for (var i = 0; i < hours.length; i++){
+        var hoursListElement = document.createElement('li');
+        hoursListElement.textContent = hours[i];
+        //second for loop iterates through the cookes per hours
+        for (var j = 0; j < seaTacAirport.cookiesEachHourArray.length; j++) {
+          var cookiesListElement = document.createElement('li');
+          cookiesListElement.textContent = hours[i] + ': ' + seaTacAirport.cookiesEachHourArray[i] + ' cookies';
+          List.appendChild(cookiesListElement);
+          break; //this makes it so that hour:cookies doesn't repeat 14 times each
+        }
+      }
+      //appends the total# of cookies sold per day to end of list
+      var totalDailyCookieSales = this.totalDailyCookieSales;
+      var totalSalesElement = document.createElement('li');
+      totalSalesElement.textContent = 'Total: ' + totalDailyCookieSales + ' cookies';
+      var position = document.getElementsByTagName('ul')[15];
+      List.appendChild(totalSalesElement);
+      document.body.appendChild(List);
+  },
 };
+
 seaTacAirport.salesPerHour();
+seaTacAirport.render();
 
 //Third store: Seattle Center, min cust = 11, max cust = 38, avg = 3.7
 var seattleCenter = {
@@ -85,10 +118,35 @@ var seattleCenter = {
         this.totalDailyCookieSales += cookiesPerHour;
 
       }
-    }
+    },
+    render: function() {
+      //naming my list
+      var List = document.createElement('ul');
+      List.textContent = 'Seattle Center';
+      //first for loop iterates through the hours
+      for (var i = 0; i < hours.length; i++){
+        var hoursListElement = document.createElement('li');
+        hoursListElement.textContent = hours[i];
+        //second for loop iterates through the cookes per hours
+        for (var j = 0; j < seattleCenter.cookiesEachHourArray.length; j++) {
+          var cookiesListElement = document.createElement('li');
+          cookiesListElement.textContent = hours[i] + ': ' + seattleCenter.cookiesEachHourArray[i] + ' cookies';
+          List.appendChild(cookiesListElement);
+          break; //this makes it so that hour:cookies doesn't repeat 14 times each
+        }
+      }
+      //appends the total# of cookies sold per day to end of list
+      var totalDailyCookieSales = this.totalDailyCookieSales;
+      var totalSalesElement = document.createElement('li');
+      totalSalesElement.textContent = 'Total: ' + totalDailyCookieSales + ' cookies';
+      var position = document.getElementsByTagName('ul')[15];
+      List.appendChild(totalSalesElement);
+      document.body.appendChild(List);
+  },
 };
 
 seattleCenter.salesPerHour();
+seattleCenter.render();
 
 //Fourth store: Capitol Hill, min = 20, max 38, avg = 2.3
 var capitolHill = {
@@ -106,10 +164,36 @@ var capitolHill = {
         this.custEachHourArray.push(custPerHour);
         this.totalDailyCookieSales += cookiesPerHour;
       }
-    }
+    },
+    render: function() {
+      //naming my list
+      var List = document.createElement('ul');
+      List.textContent = 'Capitol Hill';
+      //first for loop iterates through the hours
+      for (var i = 0; i < hours.length; i++){
+        var hoursListElement = document.createElement('li');
+        hoursListElement.textContent = hours[i];
+        //second for loop iterates through the cookes per hours
+        for (var j = 0; j < capitolHill.cookiesEachHourArray.length; j++) {
+          var cookiesListElement = document.createElement('li');
+          cookiesListElement.textContent = hours[i] + ': ' + capitolHill.cookiesEachHourArray[i] + ' cookies';
+          List.appendChild(cookiesListElement);
+          break; //this makes it so that hour:cookies doesn't repeat 14 times each
+        }
+      }
+      //appends the total# of cookies sold per day to end of list
+      var totalDailyCookieSales = this.totalDailyCookieSales;
+      var totalSalesElement = document.createElement('li');
+      totalSalesElement.textContent = 'Total: ' + totalDailyCookieSales + ' cookies';
+      var position = document.getElementsByTagName('ul')[15];
+      List.appendChild(totalSalesElement);
+      document.body.appendChild(List);
+  },
 };
 
 capitolHill.salesPerHour();
+capitolHill.render();
+
 
 //Fifth store: Alki, min = 2, max = 16, avg sales = 4.6
 var alki = {
@@ -127,5 +211,32 @@ var alki = {
         this.custEachHourArray.push(custPerHour);
         this.totalDailyCookieSales += cookiesPerHour;
       }
-    }
+    },
+    render: function() {
+      //naming my list
+      var List = document.createElement('ul');
+      List.textContent = 'Alki';
+      //first for loop iterates through the hours
+      for (var i = 0; i < hours.length; i++){
+        var hoursListElement = document.createElement('li');
+        hoursListElement.textContent = hours[i];
+        //second for loop iterates through the cookes per hours
+        for (var j = 0; j < alki.cookiesEachHourArray.length; j++) {
+          var cookiesListElement = document.createElement('li');
+          cookiesListElement.textContent = hours[i] + ': ' + alki.cookiesEachHourArray[i] + ' cookies';
+          List.appendChild(cookiesListElement);
+          break; //this makes it so that hour:cookies doesn't repeat 14 times each
+        }
+      }
+      //appends the total# of cookies sold per day to end of list
+      var totalDailyCookieSales = this.totalDailyCookieSales;
+      var totalSalesElement = document.createElement('li');
+      totalSalesElement.textContent = 'Total: ' + totalDailyCookieSales + ' cookies';
+      var position = document.getElementsByTagName('ul')[15];
+      List.appendChild(totalSalesElement);
+      document.body.appendChild(List);
+  },
 };
+
+alki.salesPerHour();
+alki.render();
