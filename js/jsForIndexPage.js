06080001ff26f5e541357d422e71c01fe94a4e54
JudyVue <judyvue87@gmail.com>
2016-07-31 10:42:41 -0700

    adding interim code to detached head branch

--- js/jsForIndexPage.js
+++ js/jsForIndexPage.js
@@ -1,4 +1,5 @@
 //this js page links to index.html
+'use strict.';
 
 //global variables
 
@@ -8,12 +9,35 @@ var aboutDiv = document.getElementById('aboutDiv');
 var locationsDiv = document.getElementById('locationsDiv');
 var purchaseDiv = document.getElementById('purchaseDiv');
 
+
+
 //the buttons
 var allButtons = document.getElementsByClassName('button');
+
 var homeButton = document.getElementById('homeButton');
 var aboutButton = document.getElementById('aboutButton');
 var locationsButton = document.getElementById('locationsButton');
 var purchaseButton = document.getElementById('purchaseButton');
+
+//arrays for divs and buttons
+var divArray = [homeContent, aboutDiv, locationsDiv, purchaseDiv];
+var buttonsArray = [homeButton, aboutButton, locationsButton, purchaseButton];
+
+function clickAnyButton() {
+  for (var i=0; i <divArray.length; i++) {
+    for (var j=0; j < buttonsArray.length; j++) {
+
+      //if we click a button that contains id name of the div id name, e.g. if we click the 'about' button, this function should find that the div id name also contains 'about' and displays it accordingly
+      if (divArray[i].textContent.includes (buttonsArray[j].textContent)) {
+        divArray[i].style.display = 'block';
+      }
+      }
+    }
+  }
+
+for (var j=0; j < buttonsArray.length; j++) {
+  buttonsArray[j].addEventListener('click', clickAnyButton);
+}
 
 
 //functions
@@ -55,7 +79,7 @@ function clickPurchaseButton () {
   }
 }
 
-aboutButton.addEventListener ('click', clickAboutButton);
-locationsButton.addEventListener ('click', clickLocationsButton);
-homeButton.addEventListener ('click', clickHomeButton);
-purchaseButton.addEventListener ('click', clickPurchaseButton);
+// aboutButton.addEventListener ('click', clickAboutButton);
+// locationsButton.addEventListener ('click', clickLocationsButton);
+// homeButton.addEventListener ('click', clickHomeButton);
+// purchaseButton.addEventListener ('click', clickPurchaseButton);
